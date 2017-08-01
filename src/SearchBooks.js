94e844bc0d81c120542ee01d866969ebdee2a7c8
Santhosh Nageshwar <santhoshbabun@gmail.com>
2017-07-31 22:09:48 -0700

    Adding Router and empty components

---
+++ src/SearchBooks.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class SearchBooks extends Component {
+
+	render() {
+		return (
+			<div>
+				Search Books
+			</div>
+		)
+	}
+}
+
+export default SearchBooks;
