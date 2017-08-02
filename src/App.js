ae2ca4f63de0f7c71da5fb887249d591bea4a80b
Santhosh Nageshwar <santhoshbabun@gmail.com>
2017-08-01 23:18:36 -0700

    feat:  Adds the Components and tied all together
    Components
    
    * Book - Every single Book will be represented using this component
    * BookShelf - Each of the three shelf's are shown using this component
    * SearchBooks - Search page component which handles the search call and list the Books using Book component.
    
    Rest of the changes are from the starter template.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Route } from 'react-router-dom'
-import ListBookShelf from './ListBookShelf'
+import { Route, Link } from 'react-router-dom'
 import SearchBooks from './SearchBooks'
+import BookShelf from './BookShelf'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
 
 class App extends Component {
+
+  state = {
+    bookShelfs: {
+      read: [],
+      wantToRead: [],
+      currentlyReading: []
+    }
+  }
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      let bookShelfs = {
+        read: [],
+        wantToRead: [],
+        currentlyReading: []
+      }
+      books.forEach((book) => {
+        if (bookShelfs[book.shelf]) {
+          bookShelfs[book.shelf].push(book)
+        }
+      });
+      this.setState({bookShelfs})
+    })
+  }
   render() {
+    const {bookShelfs} = this.state;
+
     return (
       <div>
         <Route exact path="/" render={() => (
-          <ListBookShelf />
+          <div className="list-books">
+            <div className="list-books-title">
+                <h1>My Book Shelf</h1>
+            </div>
+            <div className="list-books-content">
+                <div>
+                   <BookShelf books={bookShelfs['currentlyReading']} title="Currently reading" />
+                   <BookShelf books={bookShelfs['wantToRead']} title="Want to read" />
+                   <BookShelf books={bookShelfs['read']} title="Read" />
+                </div>
+            </div>
+            <div className="open-search">
+                <Link to="/search">Add a book</Link>
+            </div>
+          </div>
         )} />
         <Route exact path="/search" render={() => (
           <SearchBooks />
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,11 +1,62 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import Book from './Book'
 
 class SearchBooks extends Component {
+	state = {
+		query: '',
+		books: []
+	}
 
+	updateQuery = (query) => {
+		query = query.trim();
+		if(query) {
+			this.setState({
+				query: query.trim()
+			})
+			this.search()
+		} else {
+			this.clearQuery()
+		}
+	}
+
+	search = () => {
+		BooksAPI.search(this.state.query, 25).then((books) => {
+			books = books || []
+	      	this.setState({books})
+	    })
+	}
+
+	clearQuery = () => {
+		this.setState({
+			query: '',
+			books: []
+		})
+	}
 	render() {
+		const { books, query } = this.state
 		return (
-			<div>
-				Search Books
+			<div className="search-books">
+            	<div className="search-books-bar">
+            		<Link className="close-search" to="/">Close</Link>
+              		<div className="search-books-input-wrapper">
+                		<input 
+                			type="text"
+                			placeholder="Search by title or author"
+                			value={query}
+                			onChange={(event) => this.updateQuery(event.target.value)} />
+              		</div>
+            	</div>
+            	<div className="search-books-results">
+              		<ol className="books-grid">
+              			{books.map((book) => (
+				        	<li key={book.id}>
+                            	<Book book={book} />
+                        	</li>
+				        ))}
+              		</ol>
+            	</div>
 			</div>
 		)
 	}
